bb8ebb6ac58fd7eeab1881767a8b1e7010121a3a
choudharyashby <75932895+choudharyashby@users.noreply.github.com>
2021-01-15 16:43:15 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,53 @@
+var cat,catImg;
+var mouse,mouseImg;
+var backgrounds,jungle;
+ 
+function preload() {
+    //load the images here
+    jungle.loadImage("garden.png");
+    mouseImg.loadAnimation("jerryOne.png","jerryTwo.png","jerryThree.png","jerryFour.png");
+    catImg.loadAnimation("tomOne.png","tomTwo.png","tomThree.png","tomFour.png");
+}
+
+function setup(){
+    createCanvas(400,400);
+    //create tom and jerry sprites here
+    backgrounds = createSprite(400,400);
+    backgrounds.addImage(jungle);
+
+    cat = createSprite(200,200,50,50);
+    cat.addAnimation(catImg);
+
+    mouse = createSprite(60,362,100,100);
+    mouse.addAnimation(mouseImg); //Write condition here to evalute if tom and jerry collide
+
+    }                                
+
+function draw() {
+
+    background("white");
+
+    //Write condition here to evalute if tom and jerry collide
+    if(cat.x - mouse.x < (cat.width - mouse.width)/width)
+    {
+       cat.addAnimation("catLastImage",catImg3);
+        cat.changeAnimation("catLastImage");
+            cat.velocityX = 0;
+    }
+
+    keyPressed();
+    
+    drawSprites();
+}
+
+
+function keyPressed(){
+
+  //For moving and changing animation write code here
+    if(leftcode === LEFT_ARROW){
+        cat.velocityX = -5;
+        cat.addAnimation("catRunning",catImg2);
+        cat.changeAnimation("catRunning");
+    }
+    text(mouseX + ','+ mouseY,10,45);
+}
